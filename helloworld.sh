f8d4c6c04fccadf4346ce588a8560ce32558ba3b
roguione <myjobcontacts@yahoo.com>
2023-02-07 21:58:46 -0600

    new

---
+++ helloworld.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Justin H
+# Date of latest revision: 2/7/23
+# Purpose: Print a string to the terminal
+
+
+# Main
+
+echo "i am alive"
+
+# End
