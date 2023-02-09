1ef4bc1fb2e8af517463a65f93000f4b6d085d4d
roguione <myjobcontacts@yahoo.com>
2023-02-09 12:18:52 -0600

    code 3

---
+++ code_chal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Justin H
+# Date of latest revision: 2/8/23
+# Purpose: Print a string to the terminal
+
+
+# Main
+
+ print_function () {
+  cat /var/log/auth.log
+    } 
+
+ var_change () {
+    local var1=local 1
+ }      
+
+# End
