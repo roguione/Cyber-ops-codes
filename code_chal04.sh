94eb6a92f44d9bd8c810887c547bb2ce46a8032c
roguione <myjobcontacts@yahoo.com>
2023-02-10 04:17:10 +0000

    new

---
+++ code_chal04.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 04 Ops Challenge Solution
+# Author: Justin H
+# Date of latest revision: 2/9/23
+# Purpose: Print a string to the terminal
+
+# Team: Geneva && Sierra
+
+# Main
+
+mkdir home1 home2 home3 home4
+
+path_array=("./home1/" "./home2/" ",/home3/" ",/home/")
+
+touch ${path_array[0]}file1.md 
+touch ${path_array[1]}file2.md
+touch ${path_array[2]}file2.md
+touch ${path_array[3]}file3.md
+
+# End
