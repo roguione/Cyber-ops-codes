f37cc16fdf4346a7a89c343dc97057288fb0f8a9
roguione <myjobcontacts@yahoo.com>
2023-02-20 11:34:44 -0600

    right one

--- code_chal11.ps1
+++ code_chal11.ps1
@@ -4,14 +4,20 @@
 
 
 
-- `Set-NetFirewallProfile -Profile Domain,Public -FileAndPrinterSharing Enabled`: This command enables File and Printer Sharing in both the Domain and Public firewall profiles.
+# Enable File and Printer Sharing
+Set-NetFirewallProfile -Profile Domain,Public -FileAndPrinterSharing Enabled
 
-- `New-NetFirewallRule -DisplayName "Allow ICMPv4-In" -Protocol ICMPv4 -IcmpType 8 -Enabled True`: This command creates a new firewall rule that allows incoming ICMP traffic (ping requests).
+# Allow ICMP traffic
+New-NetFirewallRule -DisplayName "Allow ICMPv4-In" -Protocol ICMPv4 -IcmpType 8 -Enabled True
 
-- `Enable-PSRemoting -Force`: This command enables remote management in PowerShell.
+# Enable Remote management
+Enable-PSRemoting -Force
 
-- `Get-AppxPackage *BloatwareName* | Remove-AppxPackage`: This command retrieves the package(s) of the specified bloatware and removes them from the system.
+# Remove bloatware
+Get-AppxPackage *BloatwareName* | Remove-AppxPackage
 
-- `Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All`: This command enables the Hyper-V feature in Windows.
+# Enable Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 
-- `Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol -All`: This command disables the SMBv1 protocol, which is known to be insecure.
+# Disable SMBv1, an insecure protocol
+Disable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol -All
